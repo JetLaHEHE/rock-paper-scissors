91f6e373f934ef3a63dc4160b60de818f0286a4c
Jet <jetagatep@gmail.com>
2024-10-12 12:03:01 +0800

    Function playRound
    compares the two choices and created a logic that declares the winner depending on their choices

--- script.js
+++ script.js
@@ -30,7 +30,39 @@ function getUserChoice() {
     }
 } 
 
+//Create function playround
+function playRound(humanChoice, computerChoice) {
+    if(humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('You win! Rock beats Scissors')
+        humanScore ++;
+    }
+    else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You win! Paper beats Rock')
+        humanScore ++;
+    }
+    else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('You win! Scissors beats Paper')
+        humanScore ++;
+    }
 
-console.log(getComputerChoice());
+    else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+        console.log('You lose! Scissors beats Paper')
+        computerScore ++;
+    }
 
-console.log(getUserChoice());
+    else if (humanChoice === 'rock' && computerChoice === 'paper') {
+        console.log('You lose! Paper beats Rock')
+        computerScore ++;
+    }
+
+    else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+        console.log('You lose! Rock beats Scissors')
+        computerScore ++;
+    }
+    else if(humanChoice === computerChoice) {
+        console.log("It's a draw!");
+    }
+}
+
+const comp_choice = computerChoice();
+const human_choice = humanChoice();
